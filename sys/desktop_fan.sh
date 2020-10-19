8cdddda6e385000a89cad13a257c912e64af17a0
sabeiro <sabeiro@virgilio.it>
2020-10-19 13:32:41 +0200

    update

--- sys/desktop_fan.sh
+++ sys/desktop_fan.sh
@@ -32,3 +32,16 @@ sudo add-apt-repository ppa:linrunner/tlp
 sudo apt-get install tlp tlp-rdw
 sudo tlp start
 sudo tlp-stat
+
+##fan controller
+sudo apt install libkf5config-dev libkf5auth-dev libkf5package-dev libkf5declarative-dev libkf5coreaddons-dev libkf5dbusaddons-dev libkf5kcmutils-dev libkf5i18n-dev libkf5plasma-dev libqt5core5a libqt5widgets5 libqt5gui5 libqt5qml5 extra-cmake-modules qtbase5-dev libkf5notifications-dev qml-module-org-kde-kirigami2 qml-module-qtquick-dialogs qml-module-qtquick-controls2 qml-module-qtquick-layouts qml-module-qt-labs-settings qml-module-qt-labs-folderlistmodel cmake build-essential gettext  
+
+git clone https://github.com/Maldela/fancontrol-gui.git
+cd fancontrol-gui
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_KCM=on -DBUILD_PLASMOID=on
+make -j
+sudo make install
+
+
